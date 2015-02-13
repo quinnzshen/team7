f48f26d76f58020ba8597a96e17a07b23c5cb784
Quinn Z Shen <quinn.z.shen@gmail.com>
2015-02-13 15:31:43 -0800

    Working motor and servo code. Checkpoint completed.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "rtos.h"
 #include "MODSERIAL.h"
 
-MODSERIAL serial(USBTX, USBRX);
+Serial serial(USBTX, USBRX);
 
 DigitalOut led_green(LED_GREEN);
 DigitalOut led_red(LED_RED);
@@ -11,7 +11,7 @@ PwmOut led_blue(LED_BLUE);
 PwmOut my_led(PTA13);
 PwmOut my_led2(PTD5);
 PwmOut servo(PTC9);
-PwmOut servo2(PTC8);
+PwmOut motor(PTA5);
 
 void led_fade_thread(void const *args) {
   // Note that this function doesn't terminate, which is fine since it runs in
@@ -56,6 +56,7 @@ void fade_my_led(void const *args) {
 	}
 }
 
+/*
 void servo_thread(void const *args) {
 	while (1) {		
 		for (float i = .06f; i <= .09f; i += .002f) {
@@ -75,6 +76,51 @@ void servo_centered(void const *args) {
 		Thread::wait(20);
 	}
 }
+*/
+
+void servo_control(void const *args) {
+	char keyboard;
+	while(1) {
+		keyboard = serial.getc();
+		if(keyboard == 'w') // center
+		{
+			servo = 0.075f;
+		}
+		else if(keyboard == 'a') // left
+		{
+			servo = .09f;
+		}
+		else if (keyboard == 'd' ) // Right
+		{
+			servo = .06f;
+		}
+		Thread::wait(10);
+	}
+}
+void motor_control(void const *args){
+	char keyboard;
+	while(1) {
+		keyboard = serial.getc();
+		if(keyboard == 'h') // 100
+		{
+			motor = 1.0f;
+		}
+		else if(keyboard == 'j') // 50
+		{
+			motor = .5f;
+		}
+		else if (keyboard == 'k' ) // 30
+		{
+			motor = .3f;
+		}
+		else if (keyboard == 'l') // stop
+		{
+			motor = 0.0f;
+		}
+		Thread::wait(2);
+	}
+}
+	
 
 void led_blink_periodic(void const *args) {
   // Toggle the red LED when this function is called.
@@ -93,12 +139,22 @@ int main() {
 	
   // Mandatory "Hello, world!".
   serial.printf("Hello, world!\r\n");
-  // Start a thread running led_fade_thread().
+	
+	/*
+	servo = 0.075f; // Center
+	servo.period(0.020f); // Servo PWM period
+  */
+	servo = 0.075f;
+	
+	//motor.period(0.005f);
+	//motor.pulsewidth(); 
+	
+	// Start a thread running led_fade_thread().
   // Thread ledFadeThread(led_fade_thread);
 	// Thread blinkThread(blink_my_led); 
 	// Thread fadeThread(fade_my_led);
-	Thread servoThread(servo_thread);
-	Thread servoCenteredThread(servo_centered);
+	Thread servoThread(servo_control);
+	Thread motorThread(motor_control);
   // Set a timer to periodically call led_blink_periodic().
   RtosTimer ledBlinkTimer(led_blink_periodic);
   ledBlinkTimer.start(1000);
