b375c04b380b10007103134e388f7d850b835a37
quinnzshen <quinn.z.shen@gmail.com>
2015-04-16 17:01:56 -0700

    Working normalization

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include "MODSERIAL.h"
 
 /* ONLINE COMPILER */
-//#include "telemetry.h"
-//#include "telemetry-mbed.h"
+#include "telemetry.h"
+#include "telemetry-mbed.h"
 
 /* LAB COMPUTER COMPILER */
-#include "telemetry/server-cpp/telemetry.h"
-#include "telemetry/server-cpp/telemetry-mbed.h"
+//#include "telemetry/server-cpp/telemetry.h"
+//#include "telemetry/server-cpp/telemetry-mbed.h"
 
 MODSERIAL telemetry_serial (PTA2, PTA1);
 
@@ -37,9 +37,10 @@ void dynamicPERIOD();
 
 float target_velocity = 8; // average time per phase we are targetting (in ms)
 float motor_pwm = 0.1f; // 0.07
-float linescan_buffer[128];
-float track_buffer[128];
-float moving_average[128];
+uint16_t linescan_buffer[128];
+uint16_t track_buffer[128];
+uint16_t moving_average[128];
+uint16_t normalized_buffer[128];
 float exposure = 0;
 float camera_normalization[128] = {25,35,37,44,51,56,58,60,66,69,
                                    71,73,77,79,81,83,86,87,89,91,
@@ -419,9 +420,6 @@ void freescale()
     int MAXT = 129;
     int integrating = 0; 
     int i = 0;
-		float normalized_buffer[128];
-    //float moving_average[128];
-    //float track_buffer[128];
     Timer t;
     t.start();
     CLK = 0;
@@ -448,14 +446,19 @@ void freescale()
         // We are integrating
         if(integrating < MAXT)
         {
-            linescan_buffer[i] = camera1;
+            linescan_buffer[i] = (uint16_t)((float)camera1 * 65535);
             ++i;
         }
         else if(integrating == MAXT)
         {   
-            // Normalize Camera Data
-            for (int m = 0; m < 128; ++m) {
-                normalized_buffer[i] = linescan_buffer[i] * (100.0f/camera_normalization[i]);
+            // Normalize Camera Data 
+            for (int j = 0; j < 128; ++j) {
+                if((linescan_buffer[j] * (100.0f/camera_normalization[j])) > 65535){
+                    normalized_buffer[j] = 65535;
+                }
+                else{
+                    normalized_buffer[j] = linescan_buffer[j] * (100.0f/camera_normalization[j]);
+                }
             }
             
             // Low Pass Filter
@@ -517,26 +520,25 @@ void freescale()
             change = change > 0? change : -change;
             const int WIDTH_MIN = 20;
             const int WIDTH_MAX = 100;
-          if(change < 40 && ((minIndex - maxIndex) > WIDTH_MIN) && (minIndex - maxIndex <WIDTH_MAX))
-            {
+            if(change < 40 && ((minIndex - maxIndex) > WIDTH_MIN) && (minIndex - maxIndex <WIDTH_MAX)){
                 midpoint = (maxIndex + minIndex) / 2;
             }
             else
             {
                 // mid is not found
             }
+            servoControl_freescale(midpoint);
         }
         // Period is 2 us
         // We are collecting data 128 times which is total integration time to be 256 us.
         // We can either collecting more data but we are not doing ot right now
         // Anyway, we update the servo at every 256 us * 80 = 20.48 ms to be safe 
         // between 60 ~ 70
-        if(t.read_ms() >= 16 && integrating >= MAXT)//integrating > INTEGRATIONTIME)
+        if(t.read_ms() >= 16)//integrating > INTEGRATIONTIME)
         {   
             t.reset();
             integrating = 0;
             i = 0;
-            servoControl_freescale(midpoint);
         }
     }
 }
@@ -614,7 +616,10 @@ void telemetry_thread(void const *args){
     telemetry::Telemetry telemetry_obj(telemetry_hal);
     
     telemetry::Numeric<uint32_t> tele_time_ms(telemetry_obj, "time", "Time", "ms", 0);
-    telemetry::NumericArray<uint8_t, 128> tele_linescan(telemetry_obj, "linescan", "Linescan", "ADC", 0);  
+    telemetry::NumericArray<uint16_t, 128> tele_linescan(telemetry_obj, "linescan", "Linescan", "ADC", 0);
+    //telemetry::NumericArray<uint8_t, 128> tele_linescan_2(telemetry_obj, "linescan_2", "Linescan", "ADC", 0);
+    telemetry::NumericArray<uint16_t, 128> tele_normalized(telemetry_obj, "normalized_buffer", "Normalized Linescan 16bit", "ADC", 0);
+    //telemetry::NumericArray<uint8_t, 128> tele_normalized_2(telemetry_obj, "normalized_buffer_2", "Normalized Linescan 8bit", "ADC", 0);  
 
     telemetry_obj.transmit_header();
     
@@ -630,12 +635,41 @@ void telemetry_thread(void const *args){
         tele_time_ms = timer.read_ms() - start;
         
         for (int i = 0; i < 128; ++i){
-            tele_linescan[i] = (uint8_t)(linescan_buffer[i] * 255);
+            tele_linescan[i] = linescan_buffer[i];
+        }
+
+        /* 8bit linescan
+        for (int i = 0; i < 128; ++i){
+            tele_linescan_2[i] = (uint8_t)(linescan_buffer[i] * 255);
         }
         
+        for (int i = 0; i < 128; ++i){
+            if((tele_linescan_2[i] * (100.0f/camera_normalization[i])) > 255){
+                tele_normalized_2[i] = 255;
+            }
+            else{
+                tele_normalized_2[i] = tele_linescan_2[i] * (100.0f/camera_normalization[i]);
+            }
+        }
+        */
+
+        for (int i = 0; i < 128; ++i){
+            tele_normalized[i] = normalized_buffer[i];
+        }
+
+        /*
+        for (int i = 0; i < 128; ++i){
+            tele_normalized[i] = normalized_buffer[i];
+        }
+
+        for (int i = 0; i < 128; ++i){
+            tele_normalized_2[i] = normalized_buffer_2[i];
+        }
+        */
+
         telemetry_obj.do_io();
-				
-				Thread::wait(200);
+                
+        Thread::wait(200);
     }
 }
 
@@ -661,6 +695,6 @@ int main() {
     //Thread bThread(BlueSMiRF);
     Thread teleThread(telemetry_thread);
     
-    mainControl();
-    //freescale();
+    //mainControl();
+    freescale();
 }
